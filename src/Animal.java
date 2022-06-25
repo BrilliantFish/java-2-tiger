40812e19e789898f024120a1598a7834bbca1eb4
Liam Li <396896971@qq.com>
2022-06-25 14:54:56 +0800

    add Animal FelidaeFamily Tiger

---
+++ src/Animal.java
@@ -0,0 +1,11 @@
+public class Animal {
+    public Integer age;
+    public Double weight;
+
+    public void running(){
+        System.out.println("animal running");
+    }
+    public void eating(){
+        System.out.println("animal eating");
+    }
+}
---
+++ src/FelidaeFamily.java
@@ -0,0 +1,5 @@
+public class FelidaeFamily extends Animal {
+    public void sound(){
+        System.out.println("the meowing meowing sound.");
+    }
+}
